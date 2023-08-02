af977e9ca352b528fc709daa1e4b424602332002
Rafiul29 <rafiulislam4472@gmail.com>
2023-08-02 12:32:08 +0600

    todo updated

--- app/componets/EditTodoFrom.tsx
+++ app/componets/EditTodoFrom.tsx
@@ -1,10 +1,41 @@
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+const EditTodoFrom = ({ todo }: { todo: any }) => {
+  const [title, setTitle] = useState(todo?.title);
+  const [status, setStatus] = useState(todo?.status);
 
-const EditTodoFrom = ({todo}:{todo:any}) => {
-  const {_id,title,status}=todo;
-console.log(title)
+  const router = useRouter();
+
+  const handleUpdate = async (e: any) => {
+    e.preventDefault();
+
+    const res = await fetch(`/api/todos/${todo._id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ title, status }),
+    });
+    if (res.ok) {
+      console.log("todo was updated");
+      router.push("/");
+    }
+  };
+
   return (
-    <div>EditTodoFrom</div>
-  )
-}
+    <form onSubmit={handleUpdate}>
+      <input
+        onChange={(e) => setTitle(e.target.value)}
+        type="text"
+        value={title}
+      />
+      <div>
+        <input onClick={() => setStatus(!status)} type="checkbox" id="status" />
+        <label htmlFor="status"> status Changes</label>
+      </div>
+      <button type="submit">Update</button>
+    </form>
+  );
+};
 
-export default EditTodoFrom
+export default EditTodoFrom;
